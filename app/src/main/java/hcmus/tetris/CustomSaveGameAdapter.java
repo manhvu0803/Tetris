ac5e607bc56c1c8f6153de618af78754ddcf2e45
coderof19clc7 <94661526+lvmnhat19@users.noreply.github.com>
2022-04-15 14:39:04 +0700

    add OnClickListener to RecyclerViewSaveGame

--- app/src/main/java/hcmus/tetris/CustomSaveGameAdapter.java
+++ app/src/main/java/hcmus/tetris/CustomSaveGameAdapter.java
@@ -3,7 +3,9 @@ package hcmus.tetris;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,6 +14,7 @@ import java.util.ArrayList;
 
 import hcmus.tetris.databinding.CustomSaveGameRowBinding;
 import hcmus.tetris.dto.SaveGame;
+import hcmus.tetris.listeners.ItemCLickListener;
 
 public class CustomSaveGameAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     Context context;
@@ -34,7 +37,13 @@ public class CustomSaveGameAdapter extends RecyclerView.Adapter<RecyclerView.Vie
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         ((CustomSaveGameAdapter.SaveGameViewHolder) holder).setData(saveGames.get(position));
-        System.out.println("OK2");
+
+        ((SaveGameViewHolder) holder).setItemCLickListener(new ItemCLickListener() {
+            @Override
+            public void onCLick(View view, int position) {
+                Toast.makeText(context, "You choose row " + (position + 1), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -42,17 +51,28 @@ public class CustomSaveGameAdapter extends RecyclerView.Adapter<RecyclerView.Vie
         return saveGames.size();
     }
 
-    private static class SaveGameViewHolder extends RecyclerView.ViewHolder{
+    private static class SaveGameViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private final CustomSaveGameRowBinding binding;
+        private ItemCLickListener itemCLickListener;
         public SaveGameViewHolder(CustomSaveGameRowBinding customSaveGameBinding) {
             super(customSaveGameBinding.getRoot());
             binding = customSaveGameBinding;
+            binding.getRoot().setOnClickListener(this);
         }
         @SuppressLint("SetTextI18n")
         public void setData(SaveGame saveGame)
         {
             binding.txtTime.setText(saveGame.getDateTime());
             binding.txtHighScore.setText(Long.toString(saveGame.getScore()));
+        }
+
+        public void setItemCLickListener(ItemCLickListener itemCLickListener){
+            this.itemCLickListener = itemCLickListener;
+        }
+
+        @Override
+        public void onClick(View view) {
+            itemCLickListener.onCLick(view, getAdapterPosition());
         }
     }
 }
---
+++ app/src/main/java/hcmus/tetris/listeners/ItemCLickListener.java
@@ -0,0 +1,7 @@
+package hcmus.tetris.listeners;
+
+import android.view.View;
+
+public interface ItemCLickListener {
+    void onCLick(View view, int position);
+}
